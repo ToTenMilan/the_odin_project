695e4cf8b593cf4e83e968fb3cc2cf641db0e9aa
ToTenMilan <grzegorz.od.tych.milanowskich@gmail.com>
2017-02-05 16:20:42 +0000

    Finish main features of the hangman

--- ruby/serialization/hangman/game.rb
+++ ruby/serialization/hangman/game.rb
@@ -9,6 +9,13 @@ class Game
   def play
     @password.filter('filtered_dictionary.txt')
     p @password.pick_password
+    loop do
+      puts "Choose next letter: "
+      input = gets.chomp.downcase
+      @password.check(input)
+      @password.show
+      @password.game_over?
+    end
   end
   
 end
--- ruby/serialization/hangman/password.rb
+++ ruby/serialization/hangman/password.rb
@@ -1,7 +1,9 @@
 class Password
   
   def initialize
-    @password = String.new
+    @password = Array.new
+    @password_checked = nil # for brevity
+    @chances = 9
   end
   
   def filter(dictionary)
@@ -23,19 +25,40 @@ class Password
         @password = line.chomp.split('')
       end
     end
+    @password_checked = Array.new(@password.length, "_")
     @password
   end
   
+  def show
+    puts <<-EOB
+    
+    PASSWORD: #{@password_checked[0]} #{@password_checked[1]} #{@password_checked[2]} #{@password_checked[3]} #{@password_checked[4]} #{@password_checked[5]} #{@password_checked[6]} #{@password_checked[7]} #{@password_checked[8]} #{@password_checked[9]} #{@password_checked[10]} #{@password_checked[11]}
+    
+    CHANCES: #{@chances}
+    
+    EOB
+  end
+  
+  def check(input)
+    @password.each_with_index do |letter, index|
+      if letter == input
+        @password_checked[index] = letter
+      end
+    end
+    
+    unless @password.include?(input)
+      @chances -= 1
+    end
+  end
+  
+  def game_over?
+    if @password == @password_checked
+      puts "Congratulations! You found the password!"
+      exit
+    elsif @chances == 0
+      puts "Sorry, you are out of chances. You lost."
+      exit
+    end
+  end
+  
 end
-
-# password = ""
-# random_number = rand(1..52453)
-# lines = File.readlines('filtered_dictionary.txt')
-# lines.each_with_index do |line, index|
-#   if index == random_number
-#     password = line
-#   end
-#   p password.chomp
-# end
-# p line
-# p password.chomp
