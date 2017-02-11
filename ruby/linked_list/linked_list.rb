e32c358deff600a0809a684016f4b6451ac24ec4
ToTenMilan <grzegorz.od.tych.milanowskich@gmail.com>
2017-02-11 12:16:40 +0000

    Rework LinkedList to not use array and add basic methods

--- ruby/linked_list/linked_list.rb
+++ ruby/linked_list/linked_list.rb
@@ -1,45 +1,53 @@
-require './node'
+class Node
+  attr_accessor :value, :next_node
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
 
 class LinkedList
-  attr_reader :head
-  def initialize
-    # @head = Node.new
-    @head = Node.new("Head node")
-    p @head
-    # @list[0] = @head
+  attr_accessor :head, :node
+  @@size = 0
+  
+  def initialize(head_value = nil)
+    @head = Node.new(head_value)
   end
   
-  def append(node)
-    # p self.next_node
-    # p self
-    self.next_node = node
-    # @prev_node = @list[-1]
-    # @node = Node.new(string)
-    # @list.push(@node)
-    # @prev_node.next_node = @node
+  def append(data)
+    if @head.next_node.nil?
+      self.head.next_node = Node.new(data)
+    else
+      last = ObjectSpace.each_object(Node).find do |object|
+          object.instance_variable_get(:@next_node) == nil
+        end
+      last.next_node = Node.new(data)
+    end
+    @@size += 1
   end
   
-  def prepend(node)
-    @node = Node.new
-    @node.next_node = node
-    # @list.unshift(@node)
+  def prepend(data)
+    current_head = @head
+    @head = Node.new(data)
+    @head.next_node = current_head
+    @@size += 1
   end
   
-  # def size
-  #   @list.size
-  # end
+  def size
+    @@size
+  end
   
-  # def head
-  #   @list.first
-  # end
+  # head method defined at top
   
-  # def tail
-  #   @list.last
-  # end
+  def tail
+    ObjectSpace.each_object(Node).find do |object|
+      object.instance_variable_get(:@next_node) == nil
+    end
+  end
   
-  # def at(index)
-  #   @list[index]
-  # end
+  def at(index)
+    
+  end
   
   # def pop
   #   @list.pop
@@ -59,18 +67,19 @@ class LinkedList
   
 end
 
+list = LinkedList.new("head node")
 
-list = LinkedList.new
-# p list
-# node1 = Node.new("node1")
-# p node1
-list.append(Node.new("node1"))
-p list.next_node
-# node_one = Node.new
-# node_one.value = "Hello"
-# p node_one
-# list.append_to("Hello", head)
+list.append("node1")
 
+list.append("node2")
+list.append("node3")
+p list
+p "prepending: "
+list.prepend("prepended node")
+p list
+p list.size
+p list.head
+p list.tail
 
 
 # # p list.at(0)
--- ruby/linked_list/node.rb
+++ ruby/linked_list/node.rb
@@ -1,5 +1,5 @@
 class Node
-  attr_accessor :value
+  attr_accessor :value, :next_node
   def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
