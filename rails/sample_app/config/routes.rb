5940f070b3914837b09645235724103b0d333224
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-04-15 18:38:05 +0200

    Tweak unimportant minors

--- rails/sample_app/config/routes.rb
+++ rails/sample_app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  # syntax error, unexpected => root to: => "static_pages#home" ^
   root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
--- rails/toy_app/config/routes.rb
+++ rails/toy_app/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  root 'users#index'
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
-
 
 end
