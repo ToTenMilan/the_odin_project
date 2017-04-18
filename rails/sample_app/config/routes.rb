0028f3e4805b021cf683ba178b2588c1306eabf0
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-04-18 18:35:58 +0200

    Finish layout and routes

--- rails/sample_app/config/routes.rb
+++ rails/sample_app/config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
 
   # syntax error, unexpected => root to: => "static_pages#home" ^
   root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/help', to: 'static_pages#help'# , as: 'pomidor'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
 
 end
--- rails/sample_app/test/controllers/static_pages_controller_test.rb
+++ rails/sample_app/test/controllers/static_pages_controller_test.rb
@@ -7,7 +7,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "Should get root" do
-    get root_url
+    get root_path
+    # get root_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
@@ -19,19 +20,22 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # end
 
   test "should get help" do
-    get static_pages_help_url
+    # get static_pages_help_url
+    get help_path # help_url is possible here
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    # get static_pages_about_url
+    get about_path # about_url is possible here
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    # get static_pages_contact_url
+    get contact_path # cpntact_url is possible here
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
