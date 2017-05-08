5982cd5c0f710cb584e5f36d977bbb09c865f487
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-05-08 19:37:07 +0200

    Use SSL and the Puma webserver in production

--- rails/sample_app/config/puma.rb
+++ rails/sample_app/config/puma.rb
@@ -1,3 +1,23 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/
+  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+
+#commented out by railstutorial ch. 7.5.2
+=begin
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
