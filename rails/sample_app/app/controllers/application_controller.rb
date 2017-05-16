9847f18930760282773945571e269a78c8b9d614
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-05-16 18:40:20 +0200

    Implement basic login

--- rails/sample_app/app/controllers/application_controller.rb
+++ rails/sample_app/app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: "Hello world"
-  end
+  include SessionsHelper
 end
--- rails/sample_app/app/models/user.rb
+++ rails/sample_app/app/models/user.rb
@@ -5,6 +5,13 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # digest password used in test/fixtures/users.yml
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 end
 
 # /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
