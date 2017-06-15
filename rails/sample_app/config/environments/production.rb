6c5c3040ef1e67dd3a34d3ef9f511de42ed7d203
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-06-15 19:44:29 +0200

    Add account activation (fix typo)

--- rails/sample_app/config/environments/production.rb
+++ rails/sample_app/config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'twytter.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings {
+  ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
