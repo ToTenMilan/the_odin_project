460bbfccdd75bbc5e1a8fa7f83edb9f235318f8a
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-08-10 19:56:12 +0200

    Add microposts feed

--- rails/sample_app/app/controllers/relationships_controller.rb
+++ rails/sample_app/app/controllers/relationships_controller.rb
@@ -5,8 +5,8 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:followed_id]) # find id of previewed user
     current_user.follow(@user) # add previewed user to current user's followings
     respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
+      format.html { redirect_to @user } # commenting out fail assertion in line 30
+      format.js # commenting out do not fails anything
     end
   end
 
@@ -14,8 +14,8 @@ class RelationshipsController < ApplicationController
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow(@user)
     respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
+      format.html { redirect_to @user } # commenting out fail assertion in lines 45
+      format.js # commenting out do not fails anything
     end
   end
 
--- rails/sample_app/test/integration/following_test.rb
+++ rails/sample_app/test/integration/following_test.rb
@@ -4,7 +4,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:gregor)
-    @other_user = users(:archer)
+    @other = users(:archer)
     log_in_as(@user)
   end
 
@@ -28,29 +28,29 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
   test "should follow a user the standard way" do
     assert_difference '@user.following.count', 1 do
-      post relationships_path, params: { followed_id: @other_user.id }
+      post relationships_path, params: { followed_id: @other.id }
     end
   end
 
   test "should follow a user with Ajax" do
     assert_difference '@user.following.count', 1 do
-      post relationships_path, xhr: true, params: { followed_id: @other_user.id}
+      post relationships_path, xhr: true, params: { followed_id: @other.id }
     end
   end
 
   test "should unfollow a user the standard way" do
-    @user.follow(@other_user)
-    relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
     assert_difference '@user.following.count', -1 do
       delete relationship_path(relationship)
     end
   end
 
   test "should unfollow a user with Ajax" do
-    @user.follow(@other_user)
-    relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
     assert_difference '@user.following.count', -1 do
-      delete relationship_path(relationship)
+      delete relationship_path(relationship), xhr: true
     end
   end
 end
