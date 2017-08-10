7474e983c7da67b05b72310ab135965dda376a7c
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-08-10 15:48:14 +0200

    Add web interface for following users

--- rails/sample_app/test/controllers/users_controller_test.rb
+++ rails/sample_app/test/controllers/users_controller_test.rb
@@ -30,18 +30,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-  # test "should not allow the admin attribute to be edited via the web" do
-  #   log_in_as(@other_user)
-  #   assert_not @other_user.admin?
-  #   patch user_path(@other_user), params: {
-  #     user: {
-  #       password: "password",
-  #       password_confirmation: "password",
-  #       admin: true
-  #     }
-  #   }
-  #   assert_not @other_user.FILLIN.admin?
-  # end
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: {
+        password: "password",
+        password_confirmation: "password",
+        admin: true
+      }
+    }
+    assert_not @other_user.admin?
+  end
 
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
@@ -71,6 +71,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 
 end
--- rails/sample_app/test/integration/users_profile_test.rb
+++ rails/sample_app/test/integration/users_profile_test.rb
@@ -13,11 +13,14 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
+    assert_select 'strong#following'
+    assert_select 'strong#followers'
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination'
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+
   end
 
 end
