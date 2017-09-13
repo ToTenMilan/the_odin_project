d37d36aab8ec1754f30164eccfb842f2a146e951
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-09-13 14:22:45 +0200

    Make search form functional by one param

--- rails/milan-flight-booker/app/controllers/flights_controller.rb
+++ rails/milan-flight-booker/app/controllers/flights_controller.rb
@@ -2,14 +2,36 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all
     @airport_options = Airport.all.map { |a| [ a.code, a.id ] }
-    @flights = Flight.all
+    p "@@@@@@@@@@@@@@@@22222 params flight @@@@@@@@@@@@@@@@@@"
+    p params[:flight]
+    p "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+    if params[:flight].nil?
+      @flights = Flight.all
+    else
+      # @flights = Flight.where("destination_airport_id LIKE ?", "%#{params[:flight][:to_airport_id]}%")
+      @flights = Flight.where(destination_airport_id: params[:flight][:to_airport_id])
+    end
+
+    p "!!!!!!!!!!!!!!!!!!!!!!!!!!! fights"
+    p @flights
+    p "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
 
 
-    # @flights = if params[:destination_airport]
-    #   Flight.where('destination_airport.code LIKE ?', "%#{params[:destination_airport]}%")
-    # else
+    # @flights = if params.nil?
     #   Flight.all
+    #   # Flight.where('destination_airport.code LIKE ?', "%#{params[:destination_airport]}%")}
+    # else
+    #   # params[:flight][:from_airport_id] &&
+    #   #             params[:flight][:to_airport_id] &&
+    #   #             params[:flight][:passengers] &&
+    #   #             params[:flight][:flight_start]
+    #   # p "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@2 params @@@@@@@@@@@@@@@@@@@@@@@@@"
+    #   # p params[:controller][:flights]
+    #   # p "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+    #   Airport.where('id LIKE ?', "%#{params[:flights][:from_airport_id])}%")
     # end
 
     # @flights.each do |f|
--- rails/milan-flight-booker/app/models/flight.rb
+++ rails/milan-flight-booker/app/models/flight.rb
@@ -3,6 +3,7 @@ class Flight < ApplicationRecord
   belongs_to :destination_airport, class_name: "Airport"
   belongs_to :departure_airport, class_name: "Airport"
 
+  # https://stackoverflow.com/questions/15720940/rails-format-date-in-drop-down-list-in-view
   def flight_date_formatted
     self.start.strftime("%m/%d/%Y")
   end
