e6296bbe17fed9f1ccf553ca7519b7d76df5554a
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-09-13 19:06:12 +0200

    Add minor bootstrap styling

--- rails/milan-flight-booker/app/controllers/flights_controller.rb
+++ rails/milan-flight-booker/app/controllers/flights_controller.rb
@@ -1,12 +1,12 @@
 class FlightsController < ApplicationController
   def index
-    @airports = Airport.all
     @airport_options = Airport.all.map { |a| [ a.code, a.id ] }
+    @flights_all = Flight.all
 
     @flights = if params[:flight].nil?
       Flight.all
     else
-      start_date_param = params[:flight][:flight_start].in_time_zone('UTC')
+      start_date_param = params[:flight][:start].in_time_zone('UTC')
       Flight.where(destination_airport_id: params[:flight][:to_airport_id], departure_airport_id: params[:flight][:from_airport_id], start: (start_date_param)..(start_date_param + 1.day))
     end
 
